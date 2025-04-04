c033cf304daaed6cb667ea021c03683b97688a79
Priyanka20067 <priyankam18042006@gmail.com>
2025-04-03 19:45:57 -0800

    calculater ui again  changed

--- src/screen/calculator.jsx
+++ src/screen/calculator.jsx
@@ -91,7 +91,7 @@ function TodoList({ calculatedAmount, setCalculatedAmount }) {
 
   const formatPhone = (phone) => {
     if (phone.length === 10) {
-      return (`${phone.substring(0, 3)}`)` ${phone.substring(3, 6)}-${phone.substring(6, 10)}`;
+      return `${phone.substring(0, 3)} ${phone.substring(3, 6)}-${phone.substring(6, 10)}`;
     }
     return phone;
   };
