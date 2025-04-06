9dd085bcacff37a92549db3723490d62b6596e6e
Akuttyprince <yuvarajpro213@gmail.com>
2025-04-06 22:57:43 +0530

    deleted google login

--- shop-credit-tracking/src/App.jsx
+++ shop-credit-tracking/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import Home from './components/Home';
 import CustomerForm from './components/customerform';
-import Signup from './components/signup';
 import CustomerHistory from './components/customerHistory';
 import Test from './components/test';
 
@@ -13,7 +12,6 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/main' element={<MainPage />} />
         <Route path='/customerform' element={<CustomerForm />} />
-        <Route path='/signup' element={<Signup />} />
         <Route path='/customerhistory' element={<CustomerHistory />} />
         <Route path='/test' element={<Test />} />
       </Routes>
